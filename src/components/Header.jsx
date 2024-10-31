f09302767f0ea3dd92bcd65dee63af81dced6fb7
Sayyadi68 <96723318+Sayyadi68@users.noreply.github.com>
2024-10-31 12:51:21 +0330

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Header = () => {
   return (
     <header>
-      <div className="border border-t-[0.8rem] border-[#FF4D6D] before:absolute before:w-32 before:h-8 before:top-24 before:right-0 before:content-['فروشگاه'] before:text-white before:bg-black"></div>
+      <div className="border border-t-[0.8rem] border-[#FF4D6D] before:absolute before:w-32 before:h-8 before:top-24 before:right-0 before:content-['فروشگاه-زیوران'] before:text-white before:bg-black"></div>
       <img
         src='https://pre-websites.ir/elementor/cosmetic/wp-content/uploads/2022/08/Central_Curve_shadow2-1.svg'
         alt='icon'
