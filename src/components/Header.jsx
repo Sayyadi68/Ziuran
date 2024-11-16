71034c4eb02b6235bdd1ea06a4e72aa04f711106
Sayyadi68 <96723318+Sayyadi68@users.noreply.github.com>
2024-11-16 16:53:04 +0330

    header update

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,44 +1,62 @@
 import React from 'react'
-import { FiPhoneCall } from 'react-icons/fi'
+import { FiPhoneCall, FiMapPin } from 'react-icons/fi'
 import HeaderItem from './HeaderItem'
 import Socialicons from './Socialicons'
 const Header = () => {
   return (
     <header className='border-t-[12px] border-[#FF4D6D] px-10 py-4 w-[100vw] font-extrabold font-[BYekan]'>
-      <div className='grid grid-cols-3 gap-12 items-center justify-between'>
-        {/*Header Icon*/}
-        <img
-          src='https://pre-websites.ir/elementor/cosmetic/wp-content/uploads/2022/08/Central_Curve_shadow2-1.svg'
+      
+      {/*Header Icon*/}
+      <div className='absolute top-0 right-0 translate-y-[-125px] translate-x-[80px] -z-10'>
+        <img src='https://pre-websites.ir/elementor/cosmetic/wp-content/uploads/2022/08/Central_Curve_shadow2-1.svg'
           alt='header image'
-          className='absolute top-0 right-0 translate-y-[-125px] translate-x-[80px] -z-10'
-        />
+           />
         {/*Contacts info  */}
 
-        <div className='flex justify-start gap-10 col-span-2 '>
+      </div >
+
+
+      <div className=' flex flex-row items-center  justify-between '>
+
+        <div className=''>
           <HeaderItem
             title={'فروشگاه زیوران'}
+            title_color={'#FFFFFF'}
             subtitle={'توضیحات فروشگاه'}
-            icon={<FiPhoneCall className='text-4xl' />}
+            icon={<FiPhoneCall className='text-2xl text-[#FF4D6D]' />}
           />
-          <HeaderItem
-            title={'فروشگاه زیوران'}
-            subtitle={'پشتیبانی:'}
-            icon={<FiPhoneCall className='text-4xl' />}
-          />
-          <HeaderItem
-            title={'فروشگاه زیوران'}
-            subtitle={'آدرس فروشگاه:'}
-            icon={<FiPhoneCall className='text-4xl' />}
-          />
+
         </div>
-        <div className='justify-self-end'>
+
+        <div className='flex flex-row w-[80%] justify-between items-center '>
+
+          <div className='flex flex-row gap-[4rem] justify-between items-center '>
+
+            <HeaderItem
+              title_color={'#000'}
+              subtitle={'فروشگاه زیوران'}
+              title={'پشتیبانی:'}
+              subtitle_color={'gray'}
+              icon={<FiPhoneCall className='text-2xl text-[#FF4D6D]' />}
+            />
+            <HeaderItem
+              title_color={'#000'}
+              subtitle={'فروشگاه زیوران'}
+              title={'آدرس فروشگاه:'}
+              subtitle_color={'gray'}
+              icon={<FiMapPin className='text-2xl text-[#FF4D6D]' />}
+            />
+          </div>
+
           <div>
 
 
-            <Socialicons/>
- 
+            <Socialicons />
+
           </div>
+
         </div>
+
       </div>
     </header>
   )
--- src/components/HeaderItem.jsx
+++ src/components/HeaderItem.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
-const HeaderItem = ({ title, subtitle, icon }) => {
+const HeaderItem = ({ title, subtitle, icon, title_color, subtitle_color }) => {
   return (
     <div className='flex flex-row items-end gap-2 mr-[-20px]  '>
       {/* Header text */}
       {icon}
       <p className='flex flex-col text-[16px] gap-1'>
-        <span className='font-bold text-white'>{title}</span>
-        <span>{subtitle}</span>
+        <span className='font-bold' style={{ color: title_color }}>{title}</span>
+        <span style={{ color: subtitle_color }}>{subtitle}</span>
       </p>
     </div>
   )
