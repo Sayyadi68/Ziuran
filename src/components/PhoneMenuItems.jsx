8594b6761e4d71ddb5a49771a7ecedfb83a6ce94
Sayyadi68 <96723318+Sayyadi68@users.noreply.github.com>
2024-11-19 11:10:16 +0330

    Update PhoneMenuItems.jsx

--- src/components/PhoneMenuItems.jsx
+++ src/components/PhoneMenuItems.jsx
@@ -3,7 +3,7 @@ const PhoneMenu = ({ title, icon, link }) => {
     return (
         <a  href={"http://localhost:5173/" + link} >
             <div className=' hover:cursor-pointer flex flex-row gap-3 items-center p-3 group hover:text-[#FF4D6D] hover:transition-all transition-all border-b  border-gray-300'   >
-                <div className=' text-gray-600 group-hover:text-[#FF4D6D] ' >{icon}</div>
+                <span className='text-gray-600 group-hover:text-[#FF4D6D] ' >{icon}</span>
                 <span className='font-[Byekan] text-gray-600 group-hover:text-[#FF4D6D] ' >{title}</span>
             </div>
         </a>
