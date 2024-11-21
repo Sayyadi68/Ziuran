2df4b8a5c0d99b5b8b1e2cfa9c3b1e28378909e5
Sayyadi68 <96723318+Sayyadi68@users.noreply.github.com>
2024-11-21 17:08:07 +0330

    phone menu updated , bugs fixed

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,52 +1,52 @@
-import React, { useState, useEffect, useCallback, useRef } from 'react';  
-import { FiPhoneCall, FiMapPin } from 'react-icons/fi';  
-import HeaderItem from './HeaderItem';  
-import Socialicons from './Socialicons';  
-import { Navbar, PhoneMenu } from '../components';  
+import React, { useState, useEffect, useCallback, useRef } from 'react';
+import { FiPhoneCall, FiMapPin } from 'react-icons/fi';
+import HeaderItem from './HeaderItem';
+import Socialicons from './Socialicons';
+import { Navbar, PhoneMenu } from '../components';
 
-const PcHeader = () => {  
-  return (  
-    <>  
-      <header className='border-t-[12px] border-[#FF4D6D] px-10 py-4 w-[100%] font-extrabold font-[BYekan]'>  
-        <div className='absolute top-0 right-0 translate-y-[-125px] translate-x-[80px] -z-10'>  
-          <img  
-            src='https://pre-websites.ir/elementor/cosmetic/wp-content/uploads/2022/08/Central_Curve_shadow2-1.svg'  
-            alt='header image'  
-          />  
-        </div>  
+const PcHeader = () => {
+  return (
+    <>
+      <header className='border-t-[12px] border-[#FF4D6D] px-10 py-4 w-[100%] font-extrabold font-[BYekan]'>
+        <div className='absolute top-0 right-0 translate-y-[-125px] translate-x-[80px] -z-10'>
+          <img
+            src='https://pre-websites.ir/elementor/cosmetic/wp-content/uploads/2022/08/Central_Curve_shadow2-1.svg'
+            alt='header image'
+          />
+        </div>
 
-        <div className='flex flex-row items-center justify-between'>  
-          <HeaderItem  
-            title={'فروشگاه زیوران'}  
-            title_color={'#FFFFFF'}  
-            subtitle={'فروشگاه زیبا رویان'}  
-            icon={<FiPhoneCall className='text-2xl text-[#FF4D6D]' />}  
-          />  
-          <div className='flex flex-row w-[80%] justify-between items-center'>  
-            <div className='flex flex-row gap-[4rem]'>  
-              <HeaderItem  
-                title_color={'#000'}  
-                subtitle={'فروشگاه زیوران'}  
-                title={'پشتیبانی:'}  
-                subtitle_color={'gray'}  
-                icon={<FiPhoneCall className='text-2xl text-[#FF4D6D]' />}  
-              />  
-              <HeaderItem  
-                title_color={'#000'}  
-                subtitle={'فروشگاه زیوران'}  
-                title={'آدرس فروشگاه:'}  
-                subtitle_color={'gray'}  
-                icon={<FiMapPin className='text-2xl text-[#FF4D6D]' />}  
-              />  
-            </div>  
-            <Socialicons />  
-          </div>  
-        </div>  
-      </header>  
-      <Navbar />  
-    </>  
-  );  
-};  
+        <div className='flex flex-row items-center justify-between'>
+          <HeaderItem
+            title={'فروشگاه زیوران'}
+            title_color={'#FFFFFF'}
+            subtitle={'فروشگاه زیبا رویان'}
+            icon={<FiPhoneCall className='text-2xl text-[#FF4D6D]' />}
+          />
+          <div className='flex flex-row w-[80%] justify-between items-center'>
+            <div className='flex flex-row gap-[4rem]'>
+              <HeaderItem
+                title_color={'#000'}
+                subtitle={'فروشگاه زیوران'}
+                title={'پشتیبانی:'}
+                subtitle_color={'gray'}
+                icon={<FiPhoneCall className='text-2xl text-[#FF4D6D]' />}
+              />
+              <HeaderItem
+                title_color={'#000'}
+                subtitle={'فروشگاه زیوران'}
+                title={'آدرس فروشگاه:'}
+                subtitle_color={'gray'}
+                icon={<FiMapPin className='text-2xl text-[#FF4D6D]' />}
+              />
+            </div>
+            <Socialicons />
+          </div>
+        </div>
+      </header>
+      <Navbar />
+    </>
+  );
+};
 
 const PhoneHeader = () => {  
   const [showPhoneMenu, setShowPhoneMenu] = useState(false);  
@@ -56,9 +56,13 @@ const PhoneHeader = () => {
     setShowPhoneMenu(true);  
   };  
 
+  const handleClose = () => {  
+    setShowPhoneMenu(false);  
+  };  
+
   const handleClickOutside = (event) => {  
     if (componentRef.current && !componentRef.current.contains(event.target)) {  
-      setShowPhoneMenu(false);  
+      handleClose();  
     }  
   };  
 
@@ -69,12 +73,16 @@ const PhoneHeader = () => {
     };  
   }, []);  
 
+  useEffect(() => {  
+    console.log('Phone menu state:', showPhoneMenu);  
+  }, [showPhoneMenu]);  
+
   return (  
     <>  
-      <div className="navbar bg-base-100" ref={componentRef}>  
+      <div className="navbar bg-base-100 shadow-[0_4px_3px_0px_rgba(0,0,0,0.3)]" ref={componentRef}>  
         <div className="navbar-start">  
           <div className="dropdown">  
-            <div tabIndex={0} role="button" className="btn btn-ghost btn-circle" onClick={handleShow}>  
+            <div className="btn btn-ghost btn-circle" onClick={handleShow}>  
               <svg  
                 xmlns="http://www.w3.org/2000/svg"  
                 className="h-5 w-5"  
@@ -95,7 +103,7 @@ const PhoneHeader = () => {
           <a className="btn btn-ghost text-xl">daisyUI</a>  
         </div>  
         <div className="navbar-end">  
-          <button className="btn btn-ghost btn-circle"  onClick={handleShow}>  
+          <button className="btn btn-ghost btn-circle" onClick={handleShow}>  
             <svg  
               xmlns="http://www.w3.org/2000/svg"  
               className="h-5 w-5"  
@@ -112,30 +120,35 @@ const PhoneHeader = () => {
           </button>  
         </div>  
       </div>  
-      {showPhoneMenu && <PhoneMenu className="block z-50" />}  
+
+      {showPhoneMenu && (  
+        <div className="fixed inset-0 bg-[#c0c0c069] z-[20]" onClick={handleClose}></div>  
+      )}  
+
+      <PhoneMenu className={showPhoneMenu ? "transition translate-x-0" : "block z-50 translate-x-[300px] transition ease-in-out delay-150 duration-300"} />  
     </>  
   );  
 };  
 
-const Header = () => {  
-  const [isWide, setIsWide] = useState(window.innerWidth > 1100);  
+const Header = () => {
+  const [isWide, setIsWide] = useState(window.innerWidth > 1100);
 
-  const handleResize = useCallback(() => {  
-    setIsWide(window.innerWidth > 1100);  
-  }, []);  
+  const handleResize = useCallback(() => {
+    setIsWide(window.innerWidth > 1100);
+  }, []);
 
-  useEffect(() => {  
-    window.addEventListener('resize', handleResize);  
-    return () => {  
-      window.removeEventListener('resize', handleResize);  
-    };  
-  }, [handleResize]);  
+  useEffect(() => {
+    window.addEventListener('resize', handleResize);
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, [handleResize]);
 
-  return (  
-    <>  
-      {isWide ? <PcHeader /> : <PhoneHeader />}  
-    </>  
-  );  
-};  
+  return (
+    <>
+      {isWide ? <PcHeader /> : <PhoneHeader />}
+    </>
+  );
+};
 
 export default Header;
--- src/components/PhoneMenu.jsx
+++ src/components/PhoneMenu.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState, useEffect, useCallback, useRef } from 'react';
 import {
   FiHome,
   FiShoppingBag,
@@ -10,39 +10,46 @@ import {
 } from 'react-icons/fi'
 import { PhoneMenuItems } from '.'
 
-const PhoneMenu = ({className}) => {
+const PhoneMenu = ({ className }) => {
+ 
+
   return (
-    <aside className={'fixed h-[100%] w-[300px] bg-[#FFF] z-[20] top-0 ' +  className  }>
-      <div className='flex items-center flex-row-reverse justify-between p-4 py-6 shadow-[0_4px_3px_0px_rgba(0,0,0,0.3)]'>
-        <FiSearch className='font-extrabold hover:cursor-pointer' />
-        <input
-          type='text'
-          id='small-input'
-          placeholder='جستجوی کالا'
-          className='border-0 font-extrabold font-[Byekan] text-gray-500 outline-none'
-        />
+    <>
+      <div >
+
+        <aside className={'fixed h-[100%] w-[300px] bg-[#FFF] top-0 z-[20] ' + " " +  className}>
+          <div className='flex items-center flex-row-reverse justify-between p-4 py-6 shadow-[0_4px_3px_0px_rgba(0,0,0,0.3)]'>
+            <FiSearch className='font-extrabold hover:cursor-pointer' />
+            <input
+              type='text'
+              id='small-input'
+              placeholder='جستجوی کالا'
+              className='border-0 font-extrabold font-[Byekan] text-gray-500 outline-none'
+            />
+          </div>
+
+          <PhoneMenuItems icon={<FiHome />} title={'صفحه اصلی'} link={'home'} />
+
+          <PhoneMenuItems
+            title={'فروشگاه'}
+            icon={<FiShoppingBag />}
+            link={'shop'}
+          />
+          <PhoneMenuItems title={'وبلاگ'} icon={<FiBook />} link={'blog'} />
+          <PhoneMenuItems title={'درباره ما'} icon={<FiInfo />} link={'about'} />
+          <PhoneMenuItems
+            title={'تماس با ما'}
+            icon={<FiPhoneCall />}
+            link={'call'}
+          />
+          <PhoneMenuItems
+            title={'ورود/ثبت‌ نام'}
+            icon={<FiUser />}
+            link={'login'}
+          />
+        </aside>
       </div>
-
-      <PhoneMenuItems icon={<FiHome />} title={'صفحه اصلی'} link={'home'} />
-
-      <PhoneMenuItems
-        title={'فروشگاه'}
-        icon={<FiShoppingBag />}
-        link={'shop'}
-      />
-      <PhoneMenuItems title={'وبلاگ'} icon={<FiBook />} link={'blog'} />
-      <PhoneMenuItems title={'درباره ما'} icon={<FiInfo />} link={'about'} />
-      <PhoneMenuItems
-        title={'تماس با ما'}
-        icon={<FiPhoneCall />}
-        link={'call'}
-      />
-      <PhoneMenuItems
-        title={'ورود/ثبت‌ نام'}
-        icon={<FiUser />}
-        link={'login'}
-      />
-    </aside>
+    </>
   )
 }
 export default PhoneMenu
