a1fd6ea15183a22850047a51450007fc34a7c2d6
Sayyadi68 <96723318+Sayyadi68@users.noreply.github.com>
2024-11-23 15:07:28 +0330

    said menu updated

--- src/components/PhoneMenu.jsx
+++ src/components/PhoneMenu.jsx
@@ -1,55 +1,46 @@
-import React, { useState, useEffect, useCallback, useRef } from 'react';
-import {
-  FiHome,
-  FiShoppingBag,
-  FiBook,
-  FiInfo,
-  FiPhoneCall,
-  FiUser,
-  FiSearch,
-} from 'react-icons/fi'
-import { PhoneMenuItems } from '.'
+import React from 'react';  
+import {  
+  FiHome,  
+  FiShoppingBag,  
+  FiBook,  
+  FiInfo,  
+  FiPhoneCall,  
+  FiUser,  
+  FiSearch,  
+} from 'react-icons/fi';  
+import { PhoneMenuItems } from '.';  
 
-const PhoneMenu = ({ className }) => {
- 
+const PhoneMenu = ({ className }) => {  
+  // تعریف آرایه‌ای از آیتم‌های منو  
+  const menuItems = [  
+    { title: 'صفحه اصلی', icon: <FiHome />, link: 'home' },  
+    { title: 'فروشگاه', icon: <FiShoppingBag />, link: 'shop' },  
+    { title: 'وبلاگ', icon: <FiBook />, link: 'blog' },  
+    { title: 'درباره ما', icon: <FiInfo />, link: 'about' },  
+    { title: 'تماس با ما', icon: <FiPhoneCall />, link: 'call' },  
+    { title: 'ورود/ثبت‌نام', icon: <FiUser />, link: 'login' },  
+  ];  
 
-  return (
-    <>
-      <div >
+  return (  
+    <div>  
+      <aside className={`fixed h-[100%] w-[300px] bg-[#FFF] top-0 z-[20] ${className}`}>  
+        <div className='flex items-center flex-row-reverse justify-between p-4 py-6 shadow-[0_4px_3px_0px_rgba(0,0,0,0.3)]'>  
+          <FiSearch className='font-extrabold hover:cursor-pointer' />  
+          <input  
+            type='text'  
+            id='small-input'  
+            placeholder='جستجوی کالا'  
+            className='border-0 font-extrabold font-[Byekan] text-gray-500 outline-none'  
+          />  
+        </div>  
 
-        <aside className={'fixed h-[100%] w-[300px] bg-[#FFF] top-0 z-[20] ' + " " +  className}>
-          <div className='flex items-center flex-row-reverse justify-between p-4 py-6 shadow-[0_4px_3px_0px_rgba(0,0,0,0.3)]'>
-            <FiSearch className='font-extrabold hover:cursor-pointer' />
-            <input
-              type='text'
-              id='small-input'
-              placeholder='جستجوی کالا'
-              className='border-0 font-extrabold font-[Byekan] text-gray-500 outline-none'
-            />
-          </div>
+        {/* رندر آیتم‌های منو از طریق متد map */}  
+        {menuItems.map((item, index) => (  
+          <PhoneMenuItems key={index} icon={item.icon} title={item.title} link={item.link} />  
+        ))}  
+      </aside>  
+    </div>  
+  );  
+};  
 
-          <PhoneMenuItems icon={<FiHome />} title={'صفحه اصلی'} link={'home'} />
-
-          <PhoneMenuItems
-            title={'فروشگاه'}
-            icon={<FiShoppingBag />}
-            link={'shop'}
-          />
-          <PhoneMenuItems title={'وبلاگ'} icon={<FiBook />} link={'blog'} />
-          <PhoneMenuItems title={'درباره ما'} icon={<FiInfo />} link={'about'} />
-          <PhoneMenuItems
-            title={'تماس با ما'}
-            icon={<FiPhoneCall />}
-            link={'call'}
-          />
-          <PhoneMenuItems
-            title={'ورود/ثبت‌ نام'}
-            icon={<FiUser />}
-            link={'login'}
-          />
-        </aside>
-      </div>
-    </>
-  )
-}
-export default PhoneMenu
+export default PhoneMenu;
