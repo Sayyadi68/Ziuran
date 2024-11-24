ce3592381c7771700802defb8cf5ed1bf276876a
Mujtaba <mujtabasaberi1@gmail.com>
2024-11-24 14:35:05 +0330

    products page basic setups
    کارش هنوز تموم نشده، صرفا جهت تست اینو زدم که صفحه خالی نباشه

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -1,24 +1,31 @@
-import React from "react";
+import React from 'react'
 
-const ProductCard = ({ image, name, category, oldPrice, newPrice, discount }) => {
+const ProductCard = ({
+  image,
+  name,
+  category,
+  oldPrice,
+  newPrice,
+  discount,
+}) => {
   return (
-    <div className="bg-white shadow-md rounded-lg pb-4 text-center relative cursor-pointer hover:drop-shadow-xl hover:transition-all transition-all">
+    <div className='bg-white shadow-md rounded-lg pb-4 text-center relative cursor-pointer hover:drop-shadow-xl hover:transition-all transition-all'>
       {/* نشان تخفیف */}
-      <span className="absolute right-2 bg-[#FF4D6D] text-white text-xs px-2 py-4 rounded-b-lg">
+      <span className='absolute right-2 bg-[#FF4D6D] text-white text-xs px-2 py-4 rounded-b-lg'>
         {discount}%-
       </span>
       {/* تصویر محصول */}
-      <img src={image} alt={name} className="w-[100%] h-50 mb-4 rounded-t-lg" />
+      <img src={image} alt={name} className='w-[100%] h-50 mb-4 rounded-t-lg' />
       {/* اطلاعات محصول */}
-      <h3 className="text-gray-700 font-semibold">{name}</h3>
-      <p className="text-gray-500 text-sm">{category}</p>
+      <h3 className='text-gray-700 font-semibold'>{name}</h3>
+      <p className='text-gray-500 text-sm'>{category}</p>
       {/* قیمت‌ها */}
-      <div className="mt-2">
-        <p className="text-gray-400 line-through text-sm">{oldPrice} تومان</p>
-        <p className="text-[#FF4D6D] font-bold">{newPrice} تومان</p>
+      <div className='mt-2'>
+        <p className='text-gray-400 line-through text-sm'>{oldPrice} تومان</p>
+        <p className='text-[#FF4D6D] font-bold'>{newPrice} تومان</p>
       </div>
     </div>
-  );
-};
+  )
+}
 
-export default ProductCard;
+export default ProductCard
--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -1,7 +1,57 @@
 import React from 'react'
+import productsInfo from './productsInfo'
+import { ProductCard } from '../components/index'
+const filters = [
+  'آبرسان',
+  'ادو پرفیوم',
+  'ژل شستشو',
+  'کرم پودر',
+  'کرم ضد آفتاب',
+  'ماسک صورت',
+  'مرطوب کننده',
+]
 
 const Products = () => {
-  return <div>Products</div>
+  return (
+    // این بخش قراره یه کامپونیت بشه
+    <section className='grid grid-cols-[0.6fr,2fr] gap-x-4 px-6 '>
+      <div>
+        <input
+          type='text'
+          placeholder='جستجو...'
+          className='input input-sm w-full pb-4'
+        />
+        <h2 className='font-bold text-neutral-600 text-xl'>
+          دسته بندی محصولات
+        </h2>
+        <div className='py-4 flex flex-col gap-y-4'>
+          {filters.map((filter, index) => {
+            return (
+              <div key={index} className='flex items-center gap-x-2'>
+                <input type='checkbox' className='checkbox checkbox-md' />
+                <span>{filter}</span>
+              </div>
+            )
+          })}
+        </div>
+        <h2 className='font-bold text-neutral-600 text-xl pb-4'>
+          فیلتر بر اساس قیمت
+        </h2>
+        <input
+          type='range'
+          min={0}
+          max='100'
+          value='10'
+          className='range range-error'
+        />
+      </div>
+      <div className='grid grid-cols-3 gap-x-6'>
+        {productsInfo.map((product, index) => {
+          return <ProductCard key={index} {...product} />
+        })}
+      </div>
+    </section>
+  )
 }
 
 export default Products
